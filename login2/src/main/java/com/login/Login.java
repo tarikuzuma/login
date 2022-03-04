d86e5b8e096a773f623875d602972d163d4c430c
tarikuzuma <83963273+tarikuzuma@users.noreply.github.com>
2022-03-05 03:09:24 +0800

    Add files via upload

---
+++ login2/src/main/java/com/login/Login.java
@@ -0,0 +1,43 @@
+package com.login;
+
+import java.io.IOException;
+
+import com.login.dao.LoginDao;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    
+    public Login() {
+    	super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String uname = request.getParameter("uname");
+		String pass = request.getParameter("pass");
+		
+		LoginDao dao = new LoginDao();
+		
+		if (dao.check(uname, pass))
+		{
+			HttpSession session = request.getSession();
+			session.setAttribute("username", uname);
+			response.sendRedirect("welcome.jsp");
+		} else
+		{
+			response.sendRedirect("login.jsp");
+		}
+	}
+
+
+}
